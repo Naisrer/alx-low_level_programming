bb12ed8716f195c2d3c15e4aeacbbcdface0ada8
Naisrer <nassmohh@gmail.com>
2023-02-16 15:03:11 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc 
+#include <stdio.h>
+/**
+ * main - A program that print out with put function
+ * Return: 0 (end)
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
