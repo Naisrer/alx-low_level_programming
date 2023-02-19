21db68ff2233c7b9d4470e43b294ec6cf5516050
Naisrer <nassmohh@gmail.com>
2023-02-19 10:50:10 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - possible combination of a single digit
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+	char m = '$';
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+			if (n == 9)
+				break;
+		putchar(',');
+		n++;
+	}
+	{
+		putchar(m);
+	}
+	{
+		putchar('\n');
+	}
+		return (0);
+}
