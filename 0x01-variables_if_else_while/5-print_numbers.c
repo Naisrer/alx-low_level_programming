0faeb38fe0d35b2b607c462e33f663a7393a9174
Naisrer <nassmohh@gmail.com>
2023-02-19 07:58:24 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that list alphabet
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 9; n++)
+	printf("%d\n", n);
+	return (0);
+}
