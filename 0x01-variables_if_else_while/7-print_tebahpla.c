2a391144666f22fa21ef1c235d7d2beb0f1abb7a
Naisrer <nassmohh@gmail.com>
2023-02-19 09:37:59 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that prints reversed alphabet
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char n = 'z';
+		for (n = 'z'; n >= 'a'; n--)
+		{
+			putchar(n);
+		}
+		{
+			putchar('\n');
+		}
+		return (0);
+}
