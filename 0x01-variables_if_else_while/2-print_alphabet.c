96b87a709c849d1060fbb31f85e8e977d7b789e0
Naisrer <nassmohh@gmail.com>
2023-02-19 04:40:18 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that list alphabet
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char n = 'a';
+
+	for (n = 'a'; n <= 'z'; n++)
+	putchar(n);
+	return (0);
+}
