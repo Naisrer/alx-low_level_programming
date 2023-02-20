7ce489b2b9283199ad10ed3f56852d5cf0d364d0
Naisrer <nassmohh@gmail.com>
2023-02-20 12:20:46 -0800

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - a program that prints size of a computer
+ * Return: Always (success)
+ */
 
+int main(void)
+
+{
+	printf("Size of a char: %d byte(s)");
