260a454be3618a79db6ae6e731365ed28f8dd8de
Naisrer <nassmohh@gmail.com>
2023-02-22 10:28:00 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * _islower - a program that detects lower case letter
- * Return: Always (success)
- */
-
 int _islower(int c)
 
 {
-	int c;
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
